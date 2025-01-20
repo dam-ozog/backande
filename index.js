4e7dbe68bbec4f2cf9388fa1dd63f3fea7c4e975
Damian Ożóg <dam.ozog@gmail.com>
2025-01-20 12:09:52 +0100

    message

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const app = express();
-const PORT = 4000;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+});
 
 app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
+  res.status(200).json('Welcome, in HOME');
 });
 
 app.listen(PORT, () => {
